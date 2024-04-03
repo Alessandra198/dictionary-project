c6fe16486b92096c3bf210754ac932c3e7c45613
Alessandra198 <rinaldoalessandra@gmail.com>
2024-04-03 22:03:28 +0200

    Added the form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import "./App.css";
+import DictionarySearch from "./DictionarySearch";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
+        <header>
+          <h1>Dictionary</h1>
+          <h2>What word do you want to look up?</h2>
+        </header>
+        <main>
+          <DictionarySearch />
+        </main>
+        <footer>
+          Coded with 💜 by <a href="/">Alessandra Rinaldo</a> and{" "}
+          <a href="/">open-sourced</a>
+        </footer>
+      </div>
     </div>
   );
 }
-
---
+++ src/DictionarySearch.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import "./DictionarySearch.css";
+
+export default function DictionarySearch() {
+  let [keyword, setKeyword] = useState(null);
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword}`);
+  }
+
+  function handleKeyword(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="DictionarySearch">
+      <form onSubmit={search}>
+        <input
+          type="search"
+          placeholder="Type a word..."
+          className="input-search"
+          onChange={handleKeyword}
+        />
+        <input type="submit" value="Search" className="input-button" />
+      </form>
+    </div>
+  );
+}
